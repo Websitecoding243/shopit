cc78a92a8800abcbd77d4edae741884d820f10d0
Website Coder <94606760+Websitecoding243@users.noreply.github.com>
2022-05-18 13:07:27 +0530

    File upload limit extended

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,9 @@ if (process.env.NODE_ENV !== 'PRODUCTION')
   require('dotenv').config({ path: 'backend/config/config.env' });
 // dotenv.config({ path: 'backend/config/config.env' })
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 app.use(cookieParser());
 app.use(fileUpload());
 
